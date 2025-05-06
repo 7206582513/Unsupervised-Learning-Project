83647b406f27705a7ea1ba7f25f4332b663f3091
7206582513 <rohitmakani564@gmail.com>
2025-05-06 10:08:37 +0530

    Change Port

--- app.py
+++ app.py
@@ -26,6 +26,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
